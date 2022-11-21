ae9660dab25c8e36384b7470c0f3c485a91f6435
lb <74695114+louayboughattas@users.noreply.github.com>
2022-11-21 22:33:43 +0100

    m4

--- connection.inc.php
+++ connection.inc.php
@@ -2,7 +2,7 @@
 session_start();
 $con=mysqli_connect("localhost","phpmyadmin","kali","ecom3");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'ecom3/');
-define('SITE_PATH','http://127.0.0.1/ecom3/');
+define('SITE_PATH','http://145.14.158.58/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
