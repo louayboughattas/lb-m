1bda370803cf99ca74cbe321c3ec31ac47324cbc
lb <74695114+louayboughattas@users.noreply.github.com>
2022-11-21 23:07:17 +0100

    m5-1

--- index.php
+++ index.php
@@ -1,67 +1,14 @@
 <?php require('top.php')?>
 <div class="body__overlay"></div>
         
-        <!-- Start Slider Area -->
-        <div class="slider__container slider--one bg__cat--3">
-            <div class="slide__container slider__activation__wrap owl-carousel">
-                <!-- Start Single Slide -->
-                <div class="single__slide animation__style01 slider__fixed--height">
-                    <div class="container">
-                        <div class="row align-items__center">
-                            <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
-                                <div class="slide">
-                                    <div class="slider__inner">
-                                        <h2>collection 2018</h2>
-                                        <h1>NICE CHAIR</h1>
-                                        <div class="cr__btn">
-                                            <a href="cart.html">Shop Now</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
-                                <div class="slide__thumb">
-                                    <img src="images/slider/fornt-img/1.png" alt="slider images">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- End Single Slide -->
-                <!-- Start Single Slide -->
-                <div class="single__slide animation__style01 slider__fixed--height">
-                    <div class="container">
-                        <div class="row align-items__center">
-                            <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
-                                <div class="slide">
-                                    <div class="slider__inner">
-                                        <h2>collection 2018</h2>
-                                        <h1>NICE CHAIR</h1>
-                                        <div class="cr__btn">
-                                            <a href="cart.html">Shop Now</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
-                                <div class="slide__thumb">
-                                    <img src="images/slider/fornt-img/2.png" alt="slider images">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- End Single Slide -->
-            </div>
-        </div>
-        <!-- Start Slider Area -->
+       
         <!-- Start Category Area -->
         <section class="htc__category__area ptb--100">
             <div class="container">
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="section__title--2 text-center">
-                            <h2 class="title__line">New Arrivals</h2>
+                            <h2 class="title__line">مباريات اليوم</h2>
                         </div>
                     </div>
                 </div>
@@ -80,18 +27,10 @@
                                             <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>" alt="product images">
                                         </a>
                                     </div>
-                                    <div class="fr__hover__info">
-										<ul class="product__action">
-											<li><a href="javascript:void(0)" onclick="wishlist_manage('<?php echo $list['id']?>','add')"><i class="icon-heart icons"></i></a></li>
-											<li><a href="javascript:void(0)" onclick="manage_cart('<?php echo $list['id']?>','add')"><i class="icon-handbag icons"></i></a></li>
-										</ul>
-									</div>
+                                    
                                     <div class="fr__product__inner">
                                         <h4><a href="product.php?id=<?php echo $list['id']?>"><?php echo $list['name']?></a></h4>
-                                        <ul class="fr__pro__prize">
-                                            <li class="old__prize"><?php echo $list['mrp']?></li>
-                                            <li><?php echo $list['price']?></li>
-                                        </ul>
+                                       
                                     </div>
                                 </div>
                             </div>
@@ -109,7 +48,7 @@
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="section__title--2 text-center">
-                            <h2 class="title__line">Best Seller</h2>
+                            <h2 class="title__line">مباريات الغد</h2>
                         </div>
                     </div>
                 </div>
--- product.php
+++ product.php
@@ -20,6 +20,20 @@ if(isset($_GET['id'])){
 }
 ?>
 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+ 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+  </head>
+  <body class="p-3 m-0 border-0 bd-example">
+      
+
+
+
+
  <!-- Start Bradcaump area -->
         <div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
             <div class="ht__bradcaump__wrap">
@@ -40,6 +54,11 @@ if(isset($_GET['id'])){
                 </div>
             </div>
         </div>
+        
+         
+            <div class="ratio ratio-21x9">
+      <iframe src="<?php echo $get_product['0']['short_desc']?>" title="YouTube video" allowfullscreen=""></iframe>
+    </div>
         <!-- End Bradcaump area -->
         <!-- Start Product Details Area -->
         <section class="htc__product__details bg__white ptb--100">
@@ -53,7 +72,8 @@ if(isset($_GET['id'])){
                                 <div class="product__big__images">
                                     <div class="portfolio-full-image tab-content">
                                         <div role="tabpanel" class="tab-pane fade in active" id="img-tab-1">
-                                            <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$get_product['0']['image']?>">
+                                            <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$get_product['0']['image']?> "width="400" 
+     height="500" >
                                         </div>
                                     </div>
                                 </div>
@@ -61,82 +81,17 @@ if(isset($_GET['id'])){
                                 
                             </div>
                         </div>
-                        <div class="col-md-7 col-lg-7 col-sm-12 col-xs-12 smt-40 xmt-40">
-                            <div class="ht__product__dtl">
-                                <h2><?php echo $get_product['0']['name']?></h2>
-                                <ul  class="pro__prize">
-                                    <li class="old__prize"><?php echo $get_product['0']['mrp']?></li>
-                                    <li><?php echo $get_product['0']['price']?></li>
-                                </ul>
-                                <p class="pro__info"><?php echo $get_product['0']['short_desc']?></p>
-                                <div class="ht__pro__desc">
-                                    <div class="sin__desc">
-                                        <p><span>Availability:</span> In Stock</p>
-                                    </div>
-									<div class="sin__desc">
-                                        <p><span>Qty:</span> 
-										<select id="qty">
-											<option>1</option>
-											<option>2</option>
-											<option>3</option>
-											<option>4</option>
-											<option>5</option>
-											<option>6</option>
-											<option>7</option>
-											<option>8</option>
-											<option>9</option>
-											<option>10</option>
-										</select>
-										</p>
-                                    </div>
-                                    <div class="sin__desc align--left">
-                                        <p><span>Categories:</span></p>
-                                        <ul class="pro__cat__list">
-                                            <li><a href="#"><?php echo $get_product['0']['categories']?></a></li>
-                                        </ul>
-                                    </div>
-                                    
-                                    </div>
-									
-                                </div>
-								<a class="fr__btn" href="javascript:void(0)" onclick="manage_cart('<?php echo $get_product['0']['id']?>','add')">Add to cart</a>
-                            </div>
-                        </div>
+                        
                     </div>
                 </div>
             </div>
+           
             <!-- End Product Details Top -->
         </section>
         <!-- End Product Details Area 
-		<!-- Start Product Description -->
-        <section class="htc__produc__decription bg__white">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12">
-                        <!-- Start List And Grid View -->
-                        <ul class="pro__details__tab" role="tablist">
-                            <li role="presentation" class="description active"><a href="#description" role="tab" data-toggle="tab">description</a></li>
-                        </ul>
-                        <!-- End List And Grid View -->
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-xs-12">
-                        <div class="ht__pro__details__content">
-                            <!-- Start Single Content -->
-                            <div role="tabpanel" id="description" class="pro__single__content tab-pane fade in active">
-                                <div class="pro__tab__content__inner">
-                                    <?php echo $get_product['0']['description']?>
-                                </div>
-                            </div>
-                            <!-- End Single Content -->
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- End Product Description -->
+		
+      
+       
         
-										
-<?php require('footer.php')?>        
+        </body>									
+<?php require('')?>        
