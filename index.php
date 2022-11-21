c9ca5d7770b533cd5707081e75f31f935f9cd5d0
lb <74695114+louayboughattas@users.noreply.github.com>
2022-11-21 23:44:51 +0100

    m5-3

--- index.php
+++ index.php
@@ -42,42 +42,6 @@
             </div>
         </section>
         <!-- End Category Area -->
-        <!-- Start Product Area -->
-        <section class="ftr__product__area ptb--100">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12">
-                        <div class="section__title--2 text-center">
-                            <h2 class="title__line">مباريات الغد</h2>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="product__list clearfix mt--30">
-							<?php
-							$get_product=get_product($con,4,'','','','','yes');
-							foreach($get_product as $list){
-							?>
-                            <!-- Start Single Category -->
-                            <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
-                                <div class="category">
-                                    <div class="ht__cat__thumb">
-                                        <a href="product.php?id=<?php echo $list['id']?>">
-                                            <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>" alt="product images">
-                                        </a>
-                                    </div>
-                                    <div class="fr__product__inner">
-                                        <h4><a href="product.php?id=<?php echo $list['id']?>"><?php echo $list['name']?></a></h4>
-                                        
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- End Single Category -->
-							<?php } ?>
-                        </div>
-                </div>
-            </div>
-        </section>
-        <!-- End Product Area -->
+        
 		<input type="hidden" id="qty" value="1"/>
 <?php require('footer.php')?>        
