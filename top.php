1cfd7cf68c7f8fa8484dbb26cc0122651bd3dfbc
lb <boughattaslouay043@gmail.com>
2022-11-22 12:42:55 +0100

    m9

--- top.php
+++ top.php
@@ -84,3 +84,12 @@ $totalProduct=$obj->totalProduct();
                 </div>
             </div>
         </header>
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y6GG886CR3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Y6GG886CR3');
+</script>
