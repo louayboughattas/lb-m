0bc7feae9720fb24f6f9aa4aeb2e167c70cc3484
lb <74695114+louayboughattas@users.noreply.github.com>
2022-11-23 11:30:49 +0100

    m8-999

--- index.php
+++ index.php
@@ -94,6 +94,15 @@
         <!-- End Category Area -->
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9091965909990042"
      crossorigin="anonymous"></script>
+
+     <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y6GG886CR3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Y6GG886CR3');
+</script>
         
 		<input type="hidden" id="qty" value="1"/>
 <?php require('footer.php')?>        
