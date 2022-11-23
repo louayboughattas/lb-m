ce68bbb2acd71880f4e1dd6f28b461d4179dbfae
lb <74695114+louayboughattas@users.noreply.github.com>
2022-11-23 09:47:52 +0100

    m8-55

--- top.php
+++ top.php
@@ -34,7 +34,7 @@ $totalProduct=$obj->totalProduct();
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9091965909990042"
      crossorigin="anonymous"></script>
 </head>
-<body  style="background-color:#000000;">
+<body >
     <!--[if lt IE 8]>
         <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->  
