3b2e7b3aece26a4749b13178904ccf01e678de96
lb <74695114+louayboughattas@users.noreply.github.com>
2022-11-24 23:16:55 +0100

    m9

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
     <head>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9091965909990042"
      crossorigin="anonymous"></script>
+     
+     <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y6GG886CR3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Y6GG886CR3');
+</script>
     </head>
 <body>
 <?php require('top.php')?>
@@ -98,17 +107,9 @@
             </div>
         </section>
         <!-- End Category Area -->
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9091965909990042"
-     crossorigin="anonymous"></script>
+       
 
-     <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y6GG886CR3"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-Y6GG886CR3');
-</script>
+    
         
 		<input type="hidden" id="qty" value="1"/>
 <?php require('footer.php')?>        
